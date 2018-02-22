42021c628999294c4539e176bb2e112abe4bb318
Zach Bauer <zbauer91@gmail.com>
2018-02-22 16:39:37 -0500

    settled on hierarchy.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React, { Component, Fragment } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
+import NavBar from './components/compounds/NavBar'
+import Welcome from './components/pages/Welcome'
 
 class App extends Component {
-  render = () => {
-    ;<div class="entry">
+  render = (
+    <div class="entry">
       <NavBar user={this.props.user} />
       <main>
         <Switch>
-          <Route
-            path="/"
-            render={props => {
-              ;<Welcome {...props} />
-            }}
-          />
+          <Route path="/" render={props => <Welcome {...props} />} />
         </Switch>
       </main>
     </div>
-  }
+  )
 }
-
 export default App
